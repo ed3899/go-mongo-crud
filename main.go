68a37c12b45a853eb7a50498f2fca4045e858694
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-05-19 16:13:33 +0000

    checkpoint before adding gin

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"go-quickstart/db"
 	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var dbDisconnect func(ctx context.Context) error
@@ -12,10 +15,28 @@ func init() {
 	dbDisconnect = db.Connect()
 }
 
+type Listing struct {
+	Id string `bson:"_id"`
+	Access string `bson:"access"`
+}
+
 func main() {
 	defer func() {
 		if err := dbDisconnect(context.TODO()); err != nil {
 			log.Panicf("There was an error while disconnecting from the database: %#v", err)
 		}
 	}()
+
+	coll := db.MongoClient.Database(db.Db_name).Collection("listingsAndReviews")
+
+	var result Listing
+	err := coll.FindOne(context.TODO(), bson.D{}).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "   ")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
